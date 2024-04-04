2599f81b3fea85150d0a9b78fd09c17d5e764a72
suraj126708 <158551269+suraj126708@users.noreply.github.com>
2024-04-04 10:51:12 +0530

    Add files via upload

---
+++ headtail-game/script.js
@@ -0,0 +1,35 @@
+
+let randomimg;
+$("button").on("click", function () {
+    const randomNum = Math.floor(Math.random() * 2) + 1; 
+    randomimg = "./images/img" + randomNum + ".png";
+
+    $("img").attr("src", randomimg);
+
+    if (randomNum === 1) {
+        $("h1").text("Head Won ....");
+        animationImg();
+        animationText();
+    } else {
+        $("h1").text("Tail Won ....");
+        animationImg();
+        animationText();
+    }
+});
+
+$("#restart").on("click", () => {
+    location.reload();
+});
+
+function animationImg() {
+    $("#myImage").fadeOut(500, function() {
+        $(this).attr("src", randomimg);
+        $(this).fadeIn(500);
+    });
+}
+
+function animationText() {
+    $("h1").fadeOut(500, function () {
+        $(this).fadeIn(500);
+    });
+}
